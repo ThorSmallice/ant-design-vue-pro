2b3d72494fa22c2df61f7591a8a8fc415b90dc21
thor <912356847@qq.com>
2024-12-19 19:23:11 +0800

    补个参数

--- src/components/table/table.vue
+++ src/components/table/table.vue
@@ -214,6 +214,7 @@ const { ExportBtn } = $$(
         tableTextConfig,
         onExportSuccess,
         onExportError,
+        onExportRequestSuccess,
     })
 )
 const { source, loading, total, updateSource }: any = $$(
