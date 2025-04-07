949776a3ecaaadd9144566319dfc17f9af805e33
thor <912356847@qq.com>
2025-04-07 15:26:44 +0800

    修改macros配置

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="unplugin-vue-macros/macros-global" />
+declare global {
+    var __PKG_NAME__: string
+}
+declare module '*.vue' {
+    import { defineComponent } from 'vue'
+    const component: ReturnType<typeof defineComponent>
+    export default component
+}
--- global.d.ts
+++
@@ -1,4 +0,0 @@
-declare global {
-    var __PKG_NAME__: string
-}
-export {}
