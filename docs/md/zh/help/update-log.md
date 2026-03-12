b82885b27f53e120f9e27e633ff242a0461fc447
thor <912356847@qq.com>
2026-03-12 16:07:10 +0800

    fixbug

--- docs/md/zh/help/update-log.md
+++ docs/md/zh/help/update-log.md
@@ -1,158 +1,164 @@
 # 更新日志
 
+## v1.0.140
+
+### Bug Fixes
+
+- 修复 modal 组件centered 模式下，异步数据导致初始位置错误的问题
+
 ## v1.0.137
 
 ### Bug Fixes
 
--   修复 table cuForm/desc 弹窗的排序影响表头排序问题
+- 修复 table cuForm/desc 弹窗的排序影响表头排序问题
 
 ## v1.0.123-136
 
 ### 新增
 
--   table descItemsProps 新增 render 配置
--   table descItemsProps 新增 sort 配置
--   增加 isTree 配置项,解决树状表格 autoSize 对于 Y 值的计算问题
+- table descItemsProps 新增 render 配置
+- table descItemsProps 新增 sort 配置
+- 增加 isTree 配置项,解决树状表格 autoSize 对于 Y 值的计算问题
 
 ### Bug Fixes
 
--   更改 cuform 提交按钮的 loading 开启时机
--   修复 ciesBtn 传参污染问题
+- 更改 cuform 提交按钮的 loading 开启时机
+- 修复 ciesBtn 传参污染问题
 
 ## v1.0.122
 
 ### 新增
 
--   TableInstance 新增 resetCuFormModel 方法
--   TableInstance 新增 CUModalFormInstance 方法
+- TableInstance 新增 resetCuFormModel 方法
+- TableInstance 新增 CUModalFormInstance 方法
 
 ### 变更
 
--   TableInstance setCuFormModel 方法从直接替换输入对象改为逐项变更
+- TableInstance setCuFormModel 方法从直接替换输入对象改为逐项变更
 
 ## v1.0.121
 
 ### 新增
 
--   Table 删除行若是当前页最后一行,则自动切换到上一页
+- Table 删除行若是当前页最后一行,则自动切换到上一页
 
 ## v1.0.119 ~ v1.0.120
 
 ### Bug Fixes
 
--   更改 Table 组件 dataSouce 赋值逻辑,解决 autoRequest===false 情况下,调用 updataSource 不生效问题
+- 更改 Table 组件 dataSouce 赋值逻辑,解决 autoRequest===false 情况下,调用 updataSource 不生效问题
 
 ## v1.0.118
 
 ### 新增
 
--   Table setQueryFormModel 方法
+- Table setQueryFormModel 方法
 
 ### Bug Fixes
 
--   Table 监听 params 的错误
+- Table 监听 params 的错误
 
 ## v1.0.117
 
 ## Bug Fixes
 
--   修复 table datasource 赋值后，total 不更新问题
+- 修复 table datasource 赋值后，total 不更新问题
 
 ## v1.0.116
 
 ### 新增
 
--   新增 Table 组件 queryFormOperationBtnsLayout 配置项，提供三种表单操作按钮布局
+- 新增 Table 组件 queryFormOperationBtnsLayout 配置项，提供三种表单操作按钮布局
 
 ### Bug Fixes
 
--   修复 table datasource 属性不更新问题
+- 修复 table datasource 属性不更新问题
 
 ## v1.0.113~115
 
 ### Bug Fixes
 
--   修改 table datasource 更新方式
--   修改 customCiesBtnSlot 类型
--   修复 table slot 转发的 key 值问题
--   修复 table type=number 时对空值的处理
+- 修改 table datasource 更新方式
+- 修改 customCiesBtnSlot 类型
+- 修复 table slot 转发的 key 值问题
+- 修复 table type=number 时对空值的处理
 
 ## v1.0.111~112
 
 ### 新增
 
--   允许 Table 组件 columns 配置项 formItemProps.customControl /formItemProps.customRender 获取 cuform 的 edit 状态
--   Table 新增 getCuModalFormIsEdit 方法
--   Table 新增 setCuFormModel 方法
+- 允许 Table 组件 columns 配置项 formItemProps.customControl /formItemProps.customRender 获取 cuform 的 edit 状态
+- Table 新增 getCuModalFormIsEdit 方法
+- Table 新增 setCuFormModel 方法
 
 ## v1.0.110
 
 ### Bug Fixes
 
--   修复 Table 树形表头的排序问题
+- 修复 Table 树形表头的排序问题
 
 ## v1.0.108~109
 
 ### Bug Fixes
 
--   修复 Table 组件默认插槽报错问题
+- 修复 Table 组件默认插槽报错问题
 
 ## v1.0.106~107
 
 ### 新增
 
--   新增 Table 组件 queryFormItemsControlProps 配置项，支持配置查询表单公共属性
+- 新增 Table 组件 queryFormItemsControlProps 配置项，支持配置查询表单公共属性
 
 ## v1.0.105
 
 ### Bug Fixes
 
--   修复 Table 组件 columnsSorter 属性失效问题
+- 修复 Table 组件 columnsSorter 属性失效问题
 
 ## v1.0.99~104
 
 ### 新特性
 
--   Table 组件 expose resetPage 方法
--   Table 组件 expose loading 状态
+- Table 组件 expose resetPage 方法
+- Table 组件 expose loading 状态
 
 ### Bug Fixes
 
--   修复类型文件导出问题
--   修复 Table 组件详情弹窗中时间戳格式显示为空的问题
--   修复 Table 组件 CiesBtn 为空时,占位丢失问题
+- 修复类型文件导出问题
+- 修复 Table 组件详情弹窗中时间戳格式显示为空的问题
+- 修复 Table 组件 CiesBtn 为空时,占位丢失问题
 
 ## v1.0.98
 
 ### Bug Fixes
 
--   Modal 组件某些场景下位置错乱问题
+- Modal 组件某些场景下位置错乱问题
 
 ## v1.0.97
 
 ### 新特性
 
--   新增 Modal 组件，支持全屏、拖拽、最大化高度
+- 新增 Modal 组件，支持全屏、拖拽、最大化高度
 
 ## v1.0.96
 
 ### Bug Fixes
 
--   Table 组件删除计算表头宽度逻辑，回归 antd 本来的特性
+- Table 组件删除计算表头宽度逻辑，回归 antd 本来的特性
 
 ## v1.0.95
 
 ### Bug Fixes
 
--   Table 组件列头显示控件序号列显示错乱问题
+- Table 组件列头显示控件序号列显示错乱问题
 
 ### 新特性
 
--   新增 查询表单项支持时间范围参数自动回填
--   新增 单元格编辑支持时间范围类型修改
--   新增 scroll.x = 'auto',自动计算 scroll.x
+- 新增 查询表单项支持时间范围参数自动回填
+- 新增 单元格编辑支持时间范围类型修改
+- 新增 scroll.x = 'auto',自动计算 scroll.x
 
 ### 优化
 
--   单元格编辑图标显示
--   单元格编辑支持失焦、回车确认回填
+- 单元格编辑图标显示
+- 单元格编辑支持失焦、回车确认回填
--- src/components/modal/modal.vue
+++ src/components/modal/modal.vue
@@ -1,90 +1,90 @@
 <template>
-    <Modal
-        :class="[
-            draggable && $style['ant-draggable-modal'],
-            $style['ant-modal'],
-            (full || isFullScreen) && $style['ant-full-modal'],
-        ]"
-        :style="draggable && style"
-        :open="open"
-        :centered="draggable ? false : draggable"
-        :mask="mask"
-        :closable="closable"
-        :get-container="getContainer"
-        v-bind="others"
-    >
-        <template #title>
-            <div
-                @dblclick="showFullScreen && toggleFullScreen"
-                ref="dragHandleRef"
-                :class="$style['ant-draggable-modal-handle']"
-                v-if="draggable"
-            ></div>
-            <Button
-                @click="toggleFullScreen"
-                :title="isFullScreen ? '退出全屏' : '全屏'"
-                size="small"
-                type="text"
-                v-if="showFullScreen"
-                :class="$style['ant-modal-full-screen-btn']"
-            >
-                <template #icon>
-                    <FullscreenOutlined v-if="!isFullScreen" />
-                    <FullscreenExitOutlined v-else />
-                </template>
-            </Button>
-            <template v-if="slot?.title">
-                <slot name="title"></slot>
-            </template>
-            <template v-else>
-                {{ title }}
-            </template>
-        </template>
+	<Modal
+		:class="[
+			draggable && $style['ant-draggable-modal'],
+			$style['ant-modal'],
+			(full || isFullScreen) && $style['ant-full-modal'],
+		]"
+		:style="draggable && style"
+		:open="open"
+		:centered="draggable ? false : draggable"
+		:mask="mask"
+		:closable="closable"
+		:get-container="getContainer"
+		v-bind="others"
+	>
+		<template #title>
+			<div
+				@dblclick="showFullScreen && toggleFullScreen"
+				ref="dragHandleRef"
+				:class="$style['ant-draggable-modal-handle']"
+				v-if="draggable"
+			></div>
+			<Button
+				@click="toggleFullScreen"
+				:title="isFullScreen ? '退出全屏' : '全屏'"
+				size="small"
+				type="text"
+				v-if="showFullScreen"
+				:class="$style['ant-modal-full-screen-btn']"
+			>
+				<template #icon>
+					<FullscreenOutlined v-if="!isFullScreen" />
+					<FullscreenExitOutlined v-else />
+				</template>
+			</Button>
+			<template v-if="slot?.title">
+				<slot name="title"></slot>
+			</template>
+			<template v-else>
+				{{ title }}
+			</template>
+		</template>
 
-        <template v-for="slot in slots" :key="slot" v-slot:[slot]="temp">
-            <slot :name="slot" v-bind="temp || {}"></slot>
-        </template>
-        <slot name="default"></slot>
-    </Modal>
+		<template v-for="slot in slots" :key="slot" v-slot:[slot]="temp">
+			<slot :name="slot" v-bind="temp || {}"></slot>
+		</template>
+		<slot name="default"></slot>
+	</Modal>
 </template>
 
 <script setup lang="tsx">
 import { useDraggable } from '@vueuse/core'
 import { Button, Modal } from 'ant-design-vue'
 import {
-    computed,
-    nextTick,
-    onMounted,
-    onUnmounted,
-    reactive,
-    ref,
-    useCssModule,
-    useSlots,
-    useTemplateRef,
-    VNodeRef,
-    watch,
+	computed,
+	nextTick,
+	onMounted,
+	onUnmounted,
+	reactive,
+	ref,
+	useCssModule,
+	useSlots,
+	useTemplateRef,
+	VNodeRef,
+	watch,
 } from 'vue'
 import { ModalProps } from './index.type'
 import { FullscreenExitOutlined, FullscreenOutlined } from '@ant-design/icons-vue'
 const {
-    title,
-    open,
-    mask = true,
-    top = 100,
-    full,
-    draggable,
-    centered,
-    closable = true,
-    showFullScreen,
-    getContainer = () => document?.body,
-    visible,
-    ...others
+	title,
+	open,
+	mask = true,
+	top = 100,
+	full,
+	draggable,
+	centered,
+	closable = true,
+	showFullScreen,
+	getContainer = () => document?.body,
+	visible,
+	...others
 } = defineProps<ModalProps>()
 const rawSlots = defineSlots()
 const slotBlackList = ['default', 'title']
 const slot = useSlots()
 const slots = computed(() => {
-    return Object?.keys?.(rawSlots)?.filter?.((key: string) => !slotBlackList?.includes(key))
+	return Object?.keys?.(rawSlots)?.filter?.((key: string) => !slotBlackList?.includes(key))
 })
 
 const $style = useCssModule()
@@ -94,11 +94,11 @@ const isFullScreen = ref(false)
 const dragHandleRef = ref<HTMLElement>()
 
 const modalRef = computed<any>(() => {
-    const modalEl = dragHandleRef.value?.closest?.(
-        `.${$style['ant-draggable-modal']}`
-    ) as HTMLElement
+	const modalEl = dragHandleRef.value?.closest?.(
+		`.${$style['ant-draggable-modal']}`,
+	) as HTMLElement
 
-    return modalEl
+	return modalEl
 })
 
 // 动态计算包裹层类名
@@ -106,130 +106,153 @@ const windowWidth = window.innerWidth
 const windowHeight = window.innerHeight
 
 const initialPosition = reactive({
-    x: 0,
-    y: top,
+	x: 0,
+	y: top,
 })
 
 const { style } = useDraggable(modalRef, {
-    handle: dragHandleRef,
-    initialValue: initialPosition,
+	handle: dragHandleRef,
+	initialValue: initialPosition,
 })
 
-watch(
-    () => [open, centered, modalRef.value],
-    async () => {
-        await nextTick()
-        initialPosition.x = (windowWidth - modalRef.value?.offsetWidth) / 2
-        initialPosition.y = centered
-            ? (windowHeight - modalRef.value?.offsetHeight) / 2
-            : full
-            ? 0
-            : top
-    }
+let resizeObserver = new ResizeObserver(
+	([
+		{
+			borderBoxSize: [{ blockSize }],
+		},
+	]: ResizeObserverEntry[]) => {
+		initialPosition.y = centered ? (windowHeight - blockSize) / 2 : full ? 0 : top
+	},
 )
+let observerTargt: HTMLDivElement
+watch(
+	() => [open, centered, modalRef.value],
+	async () => {
+		await nextTick()
+		initialPosition.x = (windowWidth - modalRef.value?.offsetWidth) / 2
+
+		if (open && modalRef.value) {
+			observerTargt = modalRef.value
+
+			resizeObserver.observe(observerTargt, {
+				box: 'border-box',
+			})
+		} else {
+			resizeObserver.unobserve(observerTargt)
+			resizeObserver.disconnect()
+			observerTargt = undefined
+		}
+	},
+)
+onUnmounted(() => {
+	if (!observerTargt) return
+	resizeObserver.unobserve(observerTargt)
+	resizeObserver.disconnect()
+	resizeObserver = undefined
+})
 
 const toggleFullScreen = () => {
-    if (!document?.fullscreenElement) {
-        modalRef.value?.requestFullscreen?.()
-    } else {
-        document?.exitFullscreen?.()
-    }
+	if (!document?.fullscreenElement) {
+		modalRef.value?.requestFullscreen?.()
+	} else {
+		document?.exitFullscreen?.()
+	}
 }
 const onFullscreenchange = () => {
-    isFullScreen.value = document?.fullscreenElement ? true : false
+	isFullScreen.value = document?.fullscreenElement ? true : false
 }
 watch(
-    () => modalRef.value,
-    () => {
-        modalRef.value?.addEventListener('fullscreenchange', onFullscreenchange)
-    }
+	() => modalRef.value,
+	() => {
+		modalRef.value?.addEventListener('fullscreenchange', onFullscreenchange)
+	},
 )
 watch(
-    () => open,
-    () => {
-        if (!open && document?.fullscreenElement) {
-            document?.exitFullscreen?.()
-        }
-    }
+	() => open,
+	() => {
+		if (!open && document?.fullscreenElement) {
+			document?.exitFullscreen?.()
+		}
+	},
 )
 
 onUnmounted(() => {
-    modalRef.value?.removeEventListener('fullscreenchange', onFullscreenchange)
+	modalRef.value?.removeEventListener('fullscreenchange', onFullscreenchange)
 })
 </script>
 
 <style lang="scss" module>
 :global(.ant-modal-root .ant-modal-wrap) {
-    overflow: hidden;
+	overflow: hidden;
 }
 
 .ant-draggable-modal {
-    margin: 0 !important;
-    padding-bottom: 0 !important;
+	margin: 0 !important;
+	padding-bottom: 0 !important;
 
-    & .ant-draggable-modal-handle {
-        display: block;
-        background: transparent;
-        position: absolute;
-        left: 0;
-        top: 0;
-        width: 100%;
-        height: 20px;
-        cursor: move;
-        pointer-events: auto;
-        z-index: 1;
-    }
+	& .ant-draggable-modal-handle {
+		display: block;
+		background: transparent;
+		position: absolute;
+		left: 0;
+		top: 0;
+		width: 100%;
+		height: 20px;
+		cursor: move;
+		pointer-events: auto;
+		z-index: 1;
+	}
 }
 
 .ant-modal {
-    max-height: 100%;
-    overflow: hidden;
-    display: flex;
-    flex-direction: column;
+	max-height: 100%;
+	overflow: hidden;
+	display: flex;
+	flex-direction: column;
 
-    & > div:first-child {
-        max-height: 100%;
-        overflow: hidden;
-        display: flex;
-        flex-direction: column;
-    }
+	& > div:first-child {
+		max-height: 100%;
+		overflow: hidden;
+		display: flex;
+		flex-direction: column;
+	}
 
-    :global(.ant-modal-content) {
-        display: flex;
-        flex-direction: column;
-        max-height: 100%;
-        overflow: hidden;
+	:global(.ant-modal-content) {
+		display: flex;
+		flex-direction: column;
+		max-height: 100%;
+		overflow: hidden;
 
-        :global(.ant-modal-body) {
-            max-height: 100%;
-            overflow-y: auto;
-            overflow-x: hidden;
-        }
-    }
+		:global(.ant-modal-body) {
+			max-height: 100%;
+			overflow-y: auto;
+			overflow-x: hidden;
+		}
+	}
 }
 
 .ant-full-modal {
-    height: 100%;
+	height: 100%;
 
-    & > div:first-child {
-        height: 100%;
-    }
+	& > div:first-child {
+		height: 100%;
+	}
 
-    :global(.ant-modal-content) {
-        height: 100%;
+	:global(.ant-modal-content) {
+		height: 100%;
 
-        :global(.ant-modal-body) {
-            height: 100%;
-        }
-    }
+		:global(.ant-modal-body) {
+			height: 100%;
+		}
+	}
 }
 
 .ant-modal-full-screen-btn {
-    position: absolute;
-    top: 17px;
-    font-size: 12px;
-    right: 46px;
-    width: 22px;
-    height: 22px;
+	position: absolute;
+	top: 17px;
+	font-size: 12px;
+	right: 46px;
+	width: 22px;
+	height: 22px;
 }
 </style>
